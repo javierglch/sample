795f7489cfda03c857705654e96aa371c842eb34
Javier <Javier@Xavicrak-PC>
2016-01-31 15:17:34 +0100

    new commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.cpp
+ * Author: Javier
+ *
+ * Created on 31 de enero de 2016, 15:16
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    return 0;
+}
+
