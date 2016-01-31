c2f97838a98d6088a96c5b51f651a8aa563feb08
Javier <Javier@Xavicrak-PC>
2016-01-31 15:11:48 +0100

    new commit

---
+++ src/sample/Clase.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+/**
+ *
+ * @author Javier
+ */
+public class Clase {
+    public String A;
+
+    public Clase(String A) {
+        this.A = A;
+    }
+
+    @Override
+    public String toString() {
+        return "Clase{" + "A=" + A + '}';
+    }
+    
+    
+}
